64592ab7bc3da0ab6887cd353b82dd85579625fc
Barbershop Deploy <deploy@barbershop.local>
2026-08-28 22:29:57 -0400

    Add analytics support to firebase.config.ts

--- src/services/firebase.config.ts
+++ src/services/firebase.config.ts
@@ -2,6 +2,7 @@ import { initializeApp, getApps, type FirebaseApp } from 'firebase/app';
 import { getAuth as _getAuth, type FirebaseAuth } from 'firebase/auth';
 import { getFirestore as _getFirestore, type Firestore } from 'firebase/firestore';
 import { getFunctions as _getFunctions, type FirebaseFunctions } from 'firebase/functions';
+import { getAnalytics } from 'firebase/analytics';
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
@@ -9,7 +10,8 @@ const firebaseConfig = {
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 export const isFirebaseConfigured =
@@ -40,12 +42,15 @@ try {
 let _auth: FirebaseAuth | null;
 let _db: Firestore | null;
 let _functions: FirebaseFunctions | null;
+let _analytics: any;
 try { _auth = _getAuth(app); } catch (e) { /* ignored */ }
 try { _db = _getFirestore(app); } catch (e) { /* ignored */ }
 try { _functions = _getFunctions(app); } catch (e) { /* ignored */ }
+try { _analytics = getAnalytics(app); } catch (e) { /* ignored */ }
 
 export const auth = _auth;
 export const db = _db;
 export const functions = _functions;
+export const analytics = _analytics;
 
 export default app;
